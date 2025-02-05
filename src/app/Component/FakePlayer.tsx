d6ab8e4963657dc005eae87643dfdfee82f09d32
Guillaume Egret <gegret.bam@tf1.fr>
2025-02-05 12:09:42 +0100

    I'm real nice, I display the memory

--- src/app/Component/FakePlayer.tsx
+++ src/app/Component/FakePlayer.tsx
@@ -1,4 +1,5 @@
-import { Button } from "react-native";
+import React, { useEffect } from "react";
+import { Button, Text, View } from "react-native";
 
 import type { FakeEvent } from "#app/eventEmitter";
 import { eventEmitter } from "#app/eventEmitter";
@@ -25,5 +26,26 @@ export const FakePlayer = () => {
       });
     }, 50);
   };
-  return <Button title={t("Start sending events")} onPress={onPressA} />;
+
+  const [mem, setMem] = React.useState(
+    // @ts-ignore
+    window.performance.memory.usedJSHeapSize,
+  );
+
+  useEffect(() => {
+    const a = setInterval(() => {
+      // @ts-ignore
+      setMem(window.performance.memory.usedJSHeapSize);
+    }, 333);
+    return () => clearInterval(a);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <View style={{ position: "absolute", top: 0, left: 100 }}>
+        <Text>Used Memory : {mem}</Text>
+      </View>
+      <Button title={t("Start sending events")} onPress={onPressA} />
+    </React.Fragment>
+  );
 };
